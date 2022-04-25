eaf4e192877671ebfd8fc7100f3b2ade8e119139
ianjustinferris <ianjustinferris@gmail.com>
2022-04-25 18:33:26 -0400

    resume

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+var projectsBtn = document.getElementById("projectsBtn")
+
+var contactBtn = document.getElementById("contactBtn")
+
+var resumeBtn = document.getElementById("resumeBtn")
+
+var aboutBtn = document.getElementById("aboutBtn")
+
