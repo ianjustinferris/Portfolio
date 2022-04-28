4a828131b2da49f00411a299a7cddd9078dfaa98
ianjustinferris <ianjustinferris@gmail.com>
2022-04-28 14:08:14 -0400

    CSS

--- Assets/script.js
+++ Assets/script.js
@@ -18,6 +18,16 @@ var ohNiceEl = document.querySelector(".ohNice")
 
 var mainEl = document.querySelector(".main")
 
+var circleChaosEl
+
+var homeBtnEl = document.getElementById('homeBtn')
+
+
+homeBtnEl.addEventListener('mouseover',moonSet)
+
+function moonSet () {
+    circleChaosEl.classList.add("animate__animated","animate__bounceOutDown")
+}
 
 
 setTimeout(timeBomb,5500)
@@ -35,12 +45,15 @@ function wiggle () {
 
 function timeBomb () {
 
+
 let div = document.createElement('div')
 
 div.classList.add("circleChaos", "col-12")
 
 mainEl.appendChild(div)
 
+var circleChaosEl = document.querySelector(".circleChaos")
+console.log(circleChaosEl)
 let div2 = document.createElement('article')
 
 div2.classList.add("squareChaos", "col-12")
@@ -51,12 +64,15 @@ triangleEl.classList.add("triangleChaos")
 
 innerTriangleEl.classList.add("innerTriangleChaos")
 
-circleEl.style.display= 'none'
+circleEl.style.display = 'none'
 
 squareEl.classList.add("squareChaos", "container-fluid")
 
 console.log('timeBomb ran')
 
+return circleChaosEl
+
 }
 
 
+setTimeout(console.log(circleChaosEl),7000)
