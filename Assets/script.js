adc0ac9d3e9751286657f999e4c8740d6b712c6a
ianjustinferris <ianjustinferris@gmail.com>
2022-04-27 21:10:39 -0400

    testing purposes

--- Assets/script.js
+++ Assets/script.js
@@ -6,3 +6,66 @@ var resumeBtn = document.getElementById("resumeBtn")
 
 var aboutBtn = document.getElementById("aboutBtn")
 
+var triangleEl = document.querySelector(".triangle")
+
+var innerTriangleEl = document.querySelector(".innerTriangle")
+
+var circleEl = document.querySelector(".circle")
+
+var squareEl = document.querySelector(".square")
+
+var ohNiceEl = document.querySelector(".ohNice")
+
+var mainEl = document.querySelector(".main")
+
+
+hideOhNice()
+function hideOhNice () {
+   ohNiceEl.style.display = 'none'
+}
+
+setTimeout(timeBomb,5500)
+
+setTimeout(wiggle,5000)
+
+setTimeout(timeBombInit,3000)
+
+// function chaosMode () {
+
+//     triangleEl.classList.add("animate__animated", "animate__slideOutRight", "triangleChaos")
+
+// }
+
+function wiggle () {
+
+    mainEl.classList.add("animate__animated", "animate__rubberBand")
+    console.log('wiggle ran')
+   
+}
+
+
+
+function timeBombInit () {
+    
+    setTimeout(hideOhNice,2000)
+    ohNiceEl.style.display ='block'
+    ohNiceEl.classList.add("ohNiceShow","animate__animated","animate__fadeIn" )
+    console.log("i ran")
+    console.log(ohNiceEl)
+}
+
+
+function timeBomb () {
+
+triangleEl.classList.add("triangleChaos")
+
+innerTriangleEl.classList.add("innerTriangleChaos")
+
+circleEl.style.display= 'none'
+
+squareEl.classList.add("squareChaos", "container-fluid")
+
+
+}
+
+
